c7cb4339ce2e528ef5a994a58a758bba5479c687
m0rthil <t.a.n.d.o.n.m.h.t+dummygit@gmail.com>
2019-11-16 20:45:50 +0530

    add temp person

--- burger/src/App.js
+++ burger/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello</h1>
+        <Person name="Mohit" age="26">My Hobbies: Gaming</Person>
+        <Person name="Suraj" age="28" />
+        <Person name="Surbhi" age="26" />
       </div>
     );
   }
